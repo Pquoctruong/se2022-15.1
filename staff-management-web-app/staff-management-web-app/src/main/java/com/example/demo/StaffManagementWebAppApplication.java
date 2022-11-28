e240799bbdea39275218d5d0483dcc64eb32902b
Pquoctruong <phamquoctruong_t64@hus.edu.vn>
2022-11-28 23:21:40 +0700

    Commit

---
+++ staff-management-web-app/staff-management-web-app/src/main/java/com/example/demo/StaffManagementWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffManagementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffManagementWebAppApplication.class, args);
+	}
+
+}
