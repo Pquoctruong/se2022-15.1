25af902039ac8ce11e650f0437536327187c7cc1
Pquoctruong <phamquoctruong_t64@hus.edu.vn>
2022-11-28 20:42:34 +0700

    Add staff controller

--- staff-management-web-app/src/main/java/com/example/demo/controller/StaffController.java
+++ staff-management-web-app/src/main/java/com/example/demo/controller/StaffController.java
@@ -1,5 +1,22 @@
 package com.example.demo.controller;
 
-public class StaffController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.service.StaffService;
+
+
+
+@Controller
+public class StaffController {
+	@Autowired
+	private StaffService staffService;
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("listStaffs", staffService.getAllStaffs());
+		return "index";
+	}
+	
 }
